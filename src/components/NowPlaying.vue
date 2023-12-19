55a7bf22edcadff784c283a9b107ce3a734eaf1f
swbilas <swbilas@gmail.com>
2023-12-19 15:12:32 -0500

    Update NowPlaying.vue

--- src/components/NowPlaying.vue
+++ src/components/NowPlaying.vue
@@ -24,7 +24,11 @@
       
       <!-- Changed to -->
       <div class="now-playing__cover">
-        <img src="<%= BASE_URL %>spotify-logo.png">
+        <img
+          :src="spotify-logo.png"
+          :alt="spotify-logo"
+          class="now-playing__image"
+        />
       </div>
       <div class="now-playing__details">
         <h1 class="now-playing__idle-heading"><i class="fa-regular fa-face-frown-open"></i>No music is playing...</h1>
