b7a24f6b77b8d1f1741468b5ef8ac352ce3f2504
swbilas <swbilas@gmail.com>
2023-12-20 15:01:00 -0500

    Update NowPlaying.vue

--- src/components/NowPlaying.vue
+++ src/components/NowPlaying.vue
@@ -23,10 +23,10 @@
 
         <!-- Test -->
         <div class="flex-row">
-          <i class="fa-solid fa-music"></i><h3 class="now-playing__track" v-text="player.trackTitle"></h3>
+          <i class="fa-solid fa-music"></i><h1 class="now-playing__track" v-text="player.trackTitle"></h1>
         </div>
         <div class="flex-row">
-          <i class="fa-solid fa-user"></i><h3 class="now-playing__artist" v-text="getTrackArtists"></h3>
+          <i class="fa-solid fa-user"></i><h2 class="now-playing__artist" v-text="getTrackArtists"></h2>
         </div>
         <div class="flex-row">
           <i class="fa-solid fa-compact-disc"></i><h3 class="now-playing__album" v-text="player.trackAlbum.title"></h3>
