65550fa8b327ba2bd1c2983f4ad94cae9f0b114a
swbilas <swbilas@gmail.com>
2023-12-20 19:06:43 -0500

    Update NowPlaying.vue

--- src/components/NowPlaying.vue
+++ src/components/NowPlaying.vue
@@ -13,7 +13,7 @@
         />
       </div>
       <div class="now-playing__details">
-        <h1 class="now-playing__track" v-text="player.trackTitle">"player.trackTitle"</h1>
+        <h1 class="now-playing__track" v-text="player.trackTitle"></h1>
 
         <!-- Add album title -->
         <h3 class="now-playing__album" v-text="player.trackAlbum.title"></h3>
@@ -21,9 +21,9 @@
         
         <h2 class="now-playing__artists" v-text="getTrackArtists"></h2>
 
-        <!-- Test
+        <!-- Test -->
         <div class="flex-row">
-          <i class="fa-solid fa-music"></i><h1 class="now-playing__track" v-text="player.trackTitle"></h1>
+          <h1 class="now-playing__track"><i class="fa-solid fa-music"></i>{{player.trackTitle}}</h1>
         </div>
         <div class="flex-row">
           <i class="fa-solid fa-compact-disc"></i><h3 class="now-playing__album" v-text="player.trackAlbum.title"></h3>
@@ -31,7 +31,7 @@
         <div class="flex-row">
           <i class="fa-solid fa-user"></i><h2 class="now-playing__artist" v-text="getTrackArtists"></h2>
         </div>
-        End -->
+        <!-- End -->
         
       </div>
     </div>
