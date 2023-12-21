8dfd4a54e0f5a52a748d9e57b11fc7fa88cb0f8d
swbilas <swbilas@gmail.com>
2023-12-20 20:47:52 -0500

    Update NowPlaying.vue

--- src/components/NowPlaying.vue
+++ src/components/NowPlaying.vue
@@ -26,16 +26,28 @@
         <!-- Try icons as bullets for list -->
         <div class="flex-details">
           <div class="flex-track">
-            <h1 class="now-playing__track"><i class="fa-solid fa-music"></i></h1>
-            <h1 class="now-playing__track" v-text="player.trackTitle"></h1>
+            <div class="flex-icon">
+              <h1 class="now-playing__track"><i class="fa-solid fa-music"></i></h1>
+            </div>
+            <div class="flex-text">
+              <h1 class="now-playing__track" v-text="player.trackTitle"></h1>
+            </div>
           </div>
           <div class="flex-album">
-            <h3 class="now-playing__album"><i class="fa-solid fa-compact-disc"></i></h3>
-            <h3 class="now-playing__album" v-text="player.trackAlbum.title"></h3>
+            <div class="flex-icon">
+              <h3 class="now-playing__album"><i class="fa-solid fa-compact-disc"></i></h3>
+            </div>
+            <div class="flex-text">
+              <h3 class="now-playing__album" v-text="player.trackAlbum.title"></h3>
+            </div>
           </div>
           <div class="flex-artists">
-            <h2 class="now-playing__artists"><i class="fa-solid fa-user"></i></h2>
-            <h2 class="now-playing__artists" v-text="getTrackArtists"></h2>
+            <div class="flex-icon">
+              <h2 class="now-playing__artists"><i class="fa-solid fa-user"></i></h2>
+            </div>
+            <div class="flex-text">
+              <h2 class="now-playing__artists" v-text="getTrackArtists"></h2>
+            </div>
           </div>
         </div>
         <!-- End custom -->
